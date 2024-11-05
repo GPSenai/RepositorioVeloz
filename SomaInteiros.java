0863394b2d8f7c24972b9cdf4351d6d7f49370a5
Gustavo Pinheiro <0000984997@senaimgaluno.com.br>
2024-11-05 15:32:35 -0300

    Add files via upload

---
+++ SomaInteiros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SomaInteiros {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+		int st = 0;
+		for(int i = 1; i < 100; i++) {
+			st = i + i ;
+		System.out.print(st);
+		}
+	}
+
+}
